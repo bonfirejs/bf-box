b68f85c1b5dd62c6bf4e20a4ffd4eefff6ff5d21
Nathan Ward <ward@digivine.com>
2015-04-17 14:24:06 -0700

    Add ability to specify color

--- addon/components/bf-box.js
+++ addon/components/bf-box.js
@@ -3,5 +3,9 @@ import layout from '../templates/components/bf-box';
 
 export default Ember.Component.extend({
   layout: layout,
-  classNames: ['box']
+  classNames: ['box'],
+  classNameBindings: ['boxColorClass'],
+  boxColorClass: function() {
+    return this.get('color') ? 'box-' + this.get('color') : '';
+  }.property('color')
 });
